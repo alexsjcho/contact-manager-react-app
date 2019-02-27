1606457d2fb2afca81d4287616409d2008bd052b
Alex Cho <alexsjcho@gmail.com>
2019-02-27 13:41:00 -0800

    added context reducer for actions

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Header = props => {
   const { branding } = props;
   return (
-    <nav class="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
         <div>
           <a href="/" className="navbar-brand">
