d43b556123f92e66b36691088966fd8acfc23266
Alex Cho <alexsjcho@gmail.com>
2019-02-26 18:06:37 -0800

    75% done implementing bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = props => {
   const { branding } = props;
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <div>
+          <a href="/" className="navbar-brand">
+            {branding}
+          </a>
+        </div>
+        <div>
+          <ul className="nvabar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
