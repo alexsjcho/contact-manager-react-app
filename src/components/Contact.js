928316f76d07955be5b9d4a4b3fc19869444a366
Alex Cho <alexsjcho@gmail.com>
2019-02-27 12:53:07 -0800

    worked on changing states for Contacts and Contact.js files

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,29 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
     const { contact } = this.props;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{contact.name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item"> Email: {contact.email}</li>
-          <li className="list-group-item"> Phone: {contact.phone}</li>
-        </ul>
+        <h4>
+          {contact.name}{" "}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />{" "}
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item"> Email: {contact.email}</li>
+            <li className="list-group-item"> Phone: {contact.phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
