01ff408f2391f2dfabd06da7d0cad734f720df38
Alex Cho <alexsjcho@gmail.com>
2019-02-26 17:37:58 -0800

    added propTypes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
   render() {
     const { name, email, phone } = this.props;
+
     return (
       <div>
         <h4>{name}</h4>
@@ -14,4 +16,11 @@ class Contact extends Component {
     );
   }
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
+
 export default Contact;
